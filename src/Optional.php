b3f3d0ba8ad287b88bd63291acbb0989cd8c22cd
marvin255 <postbox433@gmail.com>
2022-06-30 21:36:48 +0200

    Add basic Optional implementation

---
+++ src/Optional.php
@@ -0,0 +1,144 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\Optional;
+
+use Throwable;
+
+/**
+ * A container object which may or may not contain a non-null value.
+ * If a value is present, isPresent() will return true and get() will return the value.
+ *
+ * @psalm-template TNested
+ */
+class Optional
+{
+    /**
+     * @psalm-var TNested
+     */
+    private readonly mixed $data;
+
+    private readonly bool $isPresent;
+
+    /**
+     * @psalm-param TNested $data
+     * @psalm-param bool $isPresent
+     */
+    private function __construct(mixed $data, bool $isPresent)
+    {
+        $this->data = $data;
+        $this->isPresent = $isPresent;
+    }
+
+    /**
+     * Returns an Optional with the specified present non-null value.
+     *
+     * @psalm-template T
+     * @psalm-param T $data
+     * @psalm-return (T is string ? Optional<string> : (T is int ? Optional<int> : Optional<T>))
+     */
+    public static function of(mixed $data): self
+    {
+        return new self($data, true);
+    }
+
+    /**
+     * Returns an empty Optional instance.
+     */
+    public static function empty(): self
+    {
+        return new self(null, false);
+    }
+
+    /**
+     * If a value is present, and the value matches the given predicate,
+     * return an Optional describing the value, otherwise return an empty Optional.
+     *
+     * @psalm-param callable(TNested): bool $filter
+     * @psalm-return Optional<TNested>
+     */
+    public function filter(callable $filter): self
+    {
+        if ($this->isPresent && \call_user_func($filter, $this->data)) {
+            return $this;
+        }
+
+        return self::empty();
+    }
+
+    /**
+     * If a value is present in this Optional, returns the value, otherwise throws NoSuchElementException.
+     *
+     * @throws NoSuchElementException
+     *
+     * @psalm-return TNested
+     */
+    public function get(): mixed
+    {
+        if (!$this->isPresent) {
+            throw new NoSuchElementException('There is no data set for this optional');
+        }
+
+        return $this->data;
+    }
+
+    /**
+     * Return true if there is a value present, otherwise false.
+     */
+    public function isPresent(): bool
+    {
+        return $this->isPresent;
+    }
+
+    /**
+     * If a value is present, invoke the specified consumer with the value, otherwise do nothing.
+     *
+     * @psalm-param callable(TNested): void $consumer
+     */
+    public function ifPresent(callable $consumer): void
+    {
+        if ($this->isPresent) {
+            \call_user_func($consumer, $this->data);
+        }
+    }
+
+    /**
+     * Return the value if present, otherwise return other.
+     *
+     * @psalm-param TNested $other
+     * @psalm-return TNested
+     */
+    public function orElse(mixed $other): mixed
+    {
+        return $this->isPresent ? $this->data : $other;
+    }
+
+    /**
+     * Return the value if present, otherwise invoke other and return the result of that invocation.
+     *
+     * @psalm-param callable(): TNested $other
+     * @psalm-return TNested
+     */
+    public function orElseGet(callable $other): mixed
+    {
+        return $this->isPresent ? $this->data : \call_user_func($other);
+    }
+
+    /**
+     * Return the contained value, if present, otherwise throw an exception to be created by the provided supplier.
+     *
+     * @throws Throwable
+     *
+     * @psalm-param callable(): Throwable $exceptionSupplier
+     * @psalm-return TNested
+     */
+    public function orElseThrow(callable $exceptionSupplier): mixed
+    {
+        if ($this->isPresent) {
+            return $this->data;
+        }
+
+        throw \call_user_func($exceptionSupplier);
+    }
+}
---
+++ tests/OptionalTest.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\Optional\Tests;
+
+use Marvin255\Optional\NoSuchElementException;
+use Marvin255\Optional\Optional;
+
+/**
+ * @internal
+ */
+class OptionalTest extends BaseCase
+{
+    public function testOf(): void
+    {
+        $optional = Optional::of('test');
+
+        $this->assertInstanceOf(Optional::class, $optional);
+    }
+
+    public function testEmpty(): void
+    {
+        $optional = Optional::empty();
+
+        $this->assertInstanceOf(Optional::class, $optional);
+    }
+
+    public function testGet(): void
+    {
+        $value = 'test';
+
+        $optional = Optional::of($value);
+
+        $this->assertSame($value, $optional->get());
+    }
+
+    public function testGetNoValueException(): void
+    {
+        $optional = Optional::empty();
+
+        $this->expectException(NoSuchElementException::class);
+        $optional->get();
+    }
+
+    public function testIsPresent(): void
+    {
+        $value = 'test';
+
+        $optional = Optional::of($value);
+
+        $this->assertTrue($optional->isPresent());
+    }
+
+    public function testIsNotPresent(): void
+    {
+        $optional = Optional::empty();
+
+        $this->assertFalse($optional->isPresent());
+    }
+
+    public function testFilterOk(): void
+    {
+        $value = 'qwe';
+
+        $optional = Optional::of($value)->filter(fn (string $item) => $item === $value);
+
+        $this->assertTrue($optional->isPresent());
+        $this->assertSame($value, $optional->get());
+    }
+
+    public function testFilterNotOk(): void
+    {
+        $value = 'qweqwe';
+
+        $optional = Optional::of($value)->filter(fn (string $item) => $item !== $value);
+
+        $this->assertFalse($optional->isPresent());
+    }
+
+    public function testIfPresent(): void
+    {
+        $res = false;
+
+        Optional::of('qwe')->ifPresent(
+            function (string $item) use (&$res): void {
+                $res = $item === 'qwe';
+            }
+        );
+
+        $this->assertTrue($res);
+    }
+
+    public function testIfNotPresent(): void
+    {
+        $res = false;
+
+        Optional::empty()->ifPresent(
+            function (mixed $item) use (&$res): void {
+                $res = true;
+            }
+        );
+
+        $this->assertFalse($res);
+    }
+
+    public function testOrElse(): void
+    {
+        $other = 'other';
+
+        $result = Optional::empty()->orElse($other);
+
+        $this->assertSame($other, $result);
+    }
+
+    public function testNotOrElse(): void
+    {
+        $value = 1;
+        $other = 2;
+
+        $result = Optional::of($value)->orElse($other);
+
+        $this->assertSame($value, $result);
+    }
+}
